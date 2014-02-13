fd82a5be258e177f3ede2df72919ccef35ebfdde
Duncan McNicholl <dmcnicholl@me.com>
2014-02-13 11:42:35 +0000

    Reduced dependencies and input file requirements

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Lyratic Resolution is a blog engine I wrote for two reasons: I wanted more control over my blog's appearance and features, and I wanted to learn Python. Mike Shea's [Pueblo][1] really helped me to learn, so I thought I'd stick this up so that other people might learn from it.
 
-The script expects a folder filled with [markdown][2] files and [mustache][3] templates. If you throw [scss][4] files in there too, they'll be processed as well, assuming you have [pyScss][5] installed. Speaking of assumptions: you'll need  [Markdown][6],  [mdx_smartypants][7] and [Pystache][8] in your path for the script to work at all.
+The script expects a folder filled with [markdown][2] files and [mustache][3] templates. If you throw [scss][4] files in there too, they'll be processed as well, assuming you have [pyScss][5] installed. Speaking of assumptions: you'll need  [Markdown][6] and [Pystache][7] in your path for the script to work at all. If you have [mdx_smartypants][8] available, it will also be used.
 
-Metadata is included in a [YAML][9] block at the start of each post file: `Title` and `Date` are required keys, `Tags` and `Abstract` are optional. There is an example post, as well as generic templates and a css file, in the "example input" folder. Find and replace `www.example.com`, `Blog Title` and `author`, and you'll have a fully-functioning, [Creative Commons licensed][10] blog.
+Metadata is included in a [YAML][9] block at the start of each post file: `Title`, `Date`, `Abstract` and `Tags` are the optional keys, and the script will fill in the first three automatically if they are missing. There is an example post, as well as generic templates and a css file, in the "example input" folder. Find and replace `www.example.com`, `Blog Title` and `author`, and you'll have a fully-functioning, [Creative Commons licensed][10] blog.
 
 Have fun!
 
@@ -15,7 +15,7 @@ Have fun!
 [4]: http://sass-lang.com/
 [5]: https://github.com/Kronuz/pyScss
 [6]: https://pypi.python.org/pypi/Markdown
-[7]: https://bitbucket.org/jeunice/mdx_smartypants
-[8]: https://github.com/defunkt/pystache
+[7]: https://github.com/defunkt/pystache
+[8]: https://bitbucket.org/jeunice/mdx_smartypants
 [9]: http://yaml.org/
 [10]: http://creativecommons.org/licenses/by-nc/4.0/
--- resolve.py
+++ resolve.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 #                   Lyratic Resolution: Silvertongue Edition
-#                             The Cocktail Party
-#                      A blog engine by Duncan McNicholl
-#                                  CC-BY-NC
+#                              The Throwing Nets
+#                       A blog engine by Duncan McNicholl
+#                                   CC-BY-NC
 
 #start with blog parameters
-DieM = {'input':'/path/to/input/files',
-        'output':'/path/to/output/files',
+DieM = {'input':'/path/to/input',
+        'output':'/path/to/output',
         'feedLength':12,
         'homeLength':6}
 
@@ -20,129 +20,144 @@ import shutil
 import markdown
 import pystache
 try:
-    from scss import Scss
+  from scss import Scss
 except ImportError:
-    pass
+  pass
 
-def read_file(fileName, directory):
+def read_file(fileName, input):
 #read contents of file into unicode string
-    filePath = os.path.join(directory,fileName)
-    fileContents = codecs.open(filePath,'r','utf8').read()
-    return unicode(fileContents)
+  filePath = os.path.join(input,fileName)
+  fileContents = codecs.open(filePath,'r','utf8').read()
+  return unicode(fileContents)
 
-def parse_article(fileName,directory):
+def parse_article(fileName,input):
 #read contents of file into dictionary
-    post = read_file(fileName,directory)
+  try: #use smartypants if it's installed
     md = markdown.Markdown(extensions = ['meta','smartypants'])
-    md.convert(post)
-    article = {'title' : md.Meta['title'][0],
-               'body' : md.convert(post),
-               'datestamp' : dt.strptime(md.Meta['date'][0],'%Y-%m-%d')}
-    article['date'] = article['datestamp'].strftime('%A, %d %B \'%y')
-    article['updated'] = article['datestamp'].isoformat()+'Z'
-    lowerTitle = str(article['title']).lower().translate(None,string.punctuation)
-    article['slug'] = '-'.join(lowerTitle.split())+'.html'
-    article['wordcount'] = len(article['body'].split())+1
-    article['readtime'] = str(int(article['wordcount']/200)+2)
-    try:
-        article['abstract'] = md.Meta['abstract'][0]
-        article['tags'] = list(str(md.Meta['tags'][0]).split(', '))
-        article['taglist'] = ({'tag':id} for id in article['tags'])
-    except KeyError:
-        pass
-    return article
+  except ImportError:
+    md = markdown.Markdown(extensions = ['meta'])
+  post = read_file(fileName,input)
+  article = {}
+  article['body'] = md.convert(post)
+  try: #try to get title from front matter
+    article['title'] = md.Meta['title'][0]
+  except KeyError: #use filename if not present
+    article['title'] = fileName[:fileName.index('.')]
+  try: #try to get date from front matter
+    article['datestamp'] = dt.strptime(md.Meta['date'][0],'%Y-%m-%d')
+  except KeyError: #use modified date if not present
+    modifiedTime = os.path.getmtime(os.path.join(input,fileName))
+    article['datestamp'] = dt.fromtimestamp(modifiedTime)
+  try: #try to get abstract from front matter
+    article['abstract'] = md.Meta['abstract'][0]
+  except KeyError: #use first sentence if not present
+    article['abstract'] = article['body'][:article['body'].index('.')]
+  try: #try to get tags from front matter
+    article['tags'] = list(str(md.Meta['tags'][0]).split(', '))
+    article['taglist'] = ({'tag':id} for id in article['tags'])
+  except KeyError:
+    pass
+  #then generate further useful attributes from metadata
+  article['date'] = article['datestamp'].strftime('%A, %d %B \'%y')
+  article['updated'] = article['datestamp'].isoformat()+'Z'
+  lowerTitle = str(article['title']).lower().translate(None,string.punctuation)
+  article['slug'] = '-'.join(lowerTitle.split())+'.html'
+  article['wordcount'] = len(article['body'].split())+1
+  article['readtime'] = str(int(article['wordcount']/200)+2)
+  return article
 	
 def draft_status(article):
 #determine if article is post-dated
-    return article['datestamp'] <= dt.today()
+  return article['datestamp'] <= dt.today()
 
 def sort_and_filter(articleList):
 #sorts articles chronologically and filters out post-dated articles
-    articleList.sort(key=lambda k: k['datestamp'], reverse=True)
-    articleListForPublishing = filter(draft_status,articleList)
-    return articleListForPublishing
+  articleList.sort(key=lambda k: k['datestamp'], reverse=True)
+  articleListForPublishing = filter(draft_status,articleList)
+  return articleListForPublishing
 
 def list_tags(articles):
 #list all tags used
-    tagList = []
-    for article in articles:
-        try:
-            tags = article['tags']
-            for tag in tags:
-                if tag not in tagList:
-                    tagList.append(tag)
-        except KeyError:
-            pass
-    return tagList
+  tagList = []
+  for article in articles:
+    try:
+      tags = article['tags']
+      for tag in tags:
+        if tag not in tagList:
+          tagList.append(tag)
+    except KeyError:
+      pass
+  return tagList
 
 def select_articles(tag,articles):
 #select articles with given tag
-    editedList = []
-    for article in articles:
-        try:
-            if tag in article['tags']:
-                editedList.append(article)
-        except KeyError:
-            pass
-    return editedList
+  editedList = []
+  for article in articles:
+    try:
+      if tag in article['tags']:
+        editedList.append(article)
+    except KeyError:
+      pass
+  return editedList
     
 def sassify(file,input,output):
 #process scss file using Sass
-    scss = read_file(file,input)
-    compiler = Scss()
-    css = compiler.compile(scss)
-    filePath = os.path.join(output,file[:-4]+'css')
-    codecs.open(filePath,'w','utf8').write(css)
+  scss = read_file(file,input)
+  compiler = Scss()
+  css = compiler.compile(scss)
+  filePath = os.path.join(output,file[:-4]+'css')
+  codecs.open(filePath,'w','utf8').write(css)
 
 def wrangle_files(input,output):
 #process input folder for markdown and sass files
-    draftList = []
-    fileList = os.listdir(input)
-    for file in fileList:
-        if file.endswith(('.md','.txt')):
-            article = parse_article(file,input)
-            draftList.append(article)
-        elif file.endswith('.scss'):
-            sassify(file,input,output)
-        elif file.endswith('.stache'):
-            pass
-        elif file.startswith('.'):
-            pass
-        else:
-            shutil.copy2(os.path.join(input,file),
-                         os.path.join(output,'static',file))
-    return draftList
+  draftList = []
+  fileList = os.listdir(input)
+  for file in fileList:
+    if file.endswith(('.md','.txt')):
+      article = parse_article(file,input)
+      draftList.append(article)
+    elif file.endswith('.scss'):
+      sassify(file,input,output)
+    elif file.endswith('.stache'):
+      pass
+    elif file.startswith('.'):
+      pass
+    else:
+      shutil.copy2(os.path.join(input,file),
+                   os.path.join(output,'static',file))
+  return draftList
 
-def build_page(articles,templateName,inputFolder,outputFolder,fileName):
-#turn a dictionary into a webpage
-    template = read_file(templateName,inputFolder)
-    data = {'updated':dt.isoformat(dt.now())+'Z',
-            'tag':fileName[:-5],'articles':articles}
-    html = pystache.render(template,data)
-    filePath = os.path.join(outputFolder,fileName)
-    codecs.open(filePath,'w','utf8').write(html)
+def build_page(articles,templateName,input,output,fileName):
+#turn a dictionary and template into a webpage
+  template = read_file(templateName,input)
+  data = {'updated':dt.isoformat(dt.now())+'Z',
+          'tag':fileName[:-5],'articles':articles}
+  html = pystache.render(template,data)
+  filePath = os.path.join(output,fileName)
+  codecs.open(filePath,'w','utf8').write(html)
 
 def build_site(parameters):
 #build the relevant pages and process files
-    pm = parameters
-    draftList = wrangle_files(pm['input'],pm['output'])    
-    articleList = sort_and_filter(draftList)
+  pm = parameters
+  draftList = wrangle_files(pm['input'],pm['output'])    
+  articleList = sort_and_filter(draftList)
     
-    for page in articleList:
-        build_page(page,'page.stache',pm['input'],
-                   pm['output'],page['slug'])
+  for article in articleList:
+    build_page(article,'page.stache',pm['input'],
+               pm['output'],article['slug'])
 
-    tagList = list_tags(articleList)
-    for tag in tagList:
-        editedList = select_articles(tag,articleList)
-        build_page(editedList,'tag.stache',
-                   pm['input'],pm['output'],tag+'.html')
+  tagList = list_tags(articleList)
+  for tag in tagList:
+    editedList = select_articles(tag,articleList)
+    build_page(editedList,'tag.stache',
+               pm['input'],pm['output'],tag+'.html')
 
-    build_page(articleList[:pm['homeLength']],'index.stache',
-               pm['input'],pm['output'],'index.html')
-    build_page(articleList[:pm['feedLength']],'feed.stache',
-               pm['input'],pm['output'],'feed.xml')
-    build_page(articleList,'archive.stache',
-               pm['input'],pm['output'],'archive.html')
+  build_page(articleList[:pm['homeLength']],'index.stache',
+             pm['input'],pm['output'],'index.html')
+  build_page(articleList[:pm['feedLength']],'feed.stache',
+             pm['input'],pm['output'],'feed.xml')
+  build_page(articleList,'archive.stache',
+             pm['input'],pm['output'],'archive.html')
 
-build_site(DieM)
+if __name__ == '__main__':
+  build_site(DieM)
